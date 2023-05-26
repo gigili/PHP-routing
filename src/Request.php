ac2c4ce46adf5138d40d57f6d5c690a5219c8992
Igor Ilic <github@igorilic.net>
2023-05-26 13:10:46 +0200

    Add methods for dealing with $_FILES
    Signed-off-by: Igor Ilic <github@igorilic.net>

--- src/Request.php
+++ src/Request.php
@@ -1,203 +1,236 @@
 <?php
-declare(strict_types=1);
+	declare(strict_types=1);
 
-namespace Gac\Routing;
+	namespace Gac\Routing;
 
 
-class Request
-{
-    /**
-     * @var array Request data
-     */
-    private array $data;
+	class Request
+	{
+		/**
+		 * @var array Request data
+		 */
+		private array $data;
 
-    /**
-     * Request constructor.
-     */
-    public function __construct()
-    {
-        $requestMethod = mb_strtoupper(($_SERVER['REQUEST_METHOD'] ?? ""));
-        if (isset($_SERVER['REQUEST_METHOD']) && in_array($requestMethod, [Routes::PATCH, Routes::PUT])) {
-            $input = $this->parse_patch_and_put_request_data();
-        } else {
-            $rawInput = file_get_contents('php://input');
+		/**
+		 * Request constructor.
+		 */
+		public function __construct()
+		{
+			$requestMethod = mb_strtoupper(($_SERVER['REQUEST_METHOD'] ?? ""));
+			if (isset($_SERVER['REQUEST_METHOD']) && in_array($requestMethod, [Routes::PATCH, Routes::PUT])) {
+				$input = $this->parse_patch_and_put_request_data();
+			} else {
+				$rawInput = file_get_contents('php://input');
 
-            $input = json_decode($rawInput) ?? [];
-            if (is_array($input) && count($input) == 0) {
-                mb_parse_str($rawInput, $input);
-            }
-        }
+				$input = json_decode($rawInput) ?? [];
+				if (is_array($input) && count($input) == 0) {
+					mb_parse_str($rawInput, $input);
+				}
+			}
 
-        $_REQUEST = array_merge($_REQUEST, (array)$input);
-        $this->data = $_REQUEST;
-    }
+			$_REQUEST = array_merge($_REQUEST, (array)$input);
+			$this->data = $_REQUEST;
+		}
 
-    /**
-     * Returns a value for a specified body argument
-     *
-     * @param string $key Which request body argument to be returned
-     *
-     * @return mixed Body argument value or NULL if the argument doesn't exist
-     */
-    public function get(string $key = ''): mixed
-    {
-        return $this->data[$key] ?? NULL;
-    }
+		/**
+		 * Returns a value for a specified body argument
+		 *
+		 * @param string $key Which request body argument to be returned
+		 *
+		 * @return mixed Body argument value or NULL if the argument doesn't exist
+		 */
+		public function get(string $key = ''): mixed
+		{
+			return $this->data[$key] ?? NULL;
+		}
 
-    /**
-     * Method used to check if the request object has a specified key-value pair
-     *
-     * @param string $key The key being searched for
-     * @return bool Return true if that key exists and false otherwise
-     */
-    public function has(string $key): bool
-    {
-        return isset($this->data[$key]) ?? false;
-    }
+		/**
+		 * Method used to check if the request object has a specified key-value pair
+		 *
+		 * @param string $key The key being searched for
+		 * @return bool Returns true if that key exists and false otherwise
+		 */
+		public function has(string $key): bool
+		{
+			return isset($this->data[$key]) ?? false;
+		}
 
-    /**
-     * Returns list of all the header items or a value of a specified key.
-     * It will return NULL if the specified key can't be found.
-     *
-     * @param string $key Name of a specific item in the header list to return the value for
-     *
-     * @return array|string|null List of header values or a value of a single item
-     */
-    public function header(string $key = ''): array|string|null
-    {
-        $headers = $this->get_request_headers();
-        return empty($key) ? $headers : array_filter($headers, function ($k) use ($key
-        ) {
-            /**
-             * This is to make sure we can get a match on a key as it's not guaranteed that keys will
-             * always be in uppercase/lowercase format as some clients/sdks don't respect that specification.
-             */
-            return strtolower($k) === strtolower($key);
-        }, ARRAY_FILTER_USE_KEY) ?? NULL;
-    }
+		/**
+		 * Returns list of all the header items or a value of a specified key.
+		 * It will return NULL if the specified key can't be found.
+		 *
+		 * @param string $key Name of a specific item in the header list to return the value for
+		 *
+		 * @return array|string|null List of header values or a value of a single item
+		 */
+		public function header(string $key = ''): array|string|null
+		{
+			$headers = $this->get_request_headers();
+			return empty($key) ? $headers : array_filter($headers, function ($k) use ($key
+			) {
+				/**
+				 * This is to make sure we can get a match on a key as it's not guaranteed that keys will
+				 * always be in uppercase/lowercase format as some clients/sdks don't respect that specification.
+				 */
+				return strtolower($k) === strtolower($key);
+			}, ARRAY_FILTER_USE_KEY) ?? NULL;
+		}
 
-    /**
-     * Method used to check if the header of a request contains a specified key
-     * 
-     * @param String $key Name of the header key to check for
-     * 
-     * @return bool Returns TRUE if the key exists and has value or FALSE otherwise;
-     */
-    public function has_header(string $key): bool
-    {
-        $value = $this->headers($key);
-        return !is_null($value) && (is_array($value) && count($value) > 0);
-    }
+		/**
+		 * Method used to check if the header of a request contains a specified key
+		 *
+		 * @param String $key Name of the header key to check for
+		 *
+		 * @return bool Returns TRUE if the key exists and has value or FALSE otherwise;
+		 */
+		public function has_header(string $key): bool
+		{
+			$value = $this->header($key);
+			return (is_array($value) && count($value) > 0);
+		}
 
-    /**
-     * Method used for getting all request headers
-     *
-     * @return array It will return an array containing all the header values or an empty array
-     */
-    private function get_request_headers(): array
-    {
-        if (function_exists("apache_request_headers")) {
-            $headers = apache_request_headers() ?? NULL;
-            return $headers ?? [];
-        }
+		/**
+		 * Method used for accessing an array of files that are being uploaded
+		 *
+		 * @return array List of files that are being uploaded
+		 */
+		public function files(): array
+		{
+			return $_FILES ?? [];
+		}
 
-        $headers = [];
-        foreach ($_SERVER as $key => $value) {
-            if (str_starts_with("HTTP_", $key)) {
-                $k = str_replace("HTTP_", "", $key);
-                $headers[$k] = $value;
-            }
-        }
+		/**
+		 * Method used for getting a single file that is being uploaded
+		 * @param String $name Name of the key (usually form input name attribute)
+		 *
+		 * @return array Returns $_FILE array of information for a specified key or an empty array
+		 */
+		public function file(string $name): array
+		{
+			return $_FILES[$name] ?? [];
+		}
 
-        return $headers;
-    }
+		/**
+		 * Method used for checking if the file being uploaded is available in the Request class
+		 *
+		 * @param String $name Name of a specific file, empty if checking for any file
+		 *
+		 * @return bool Returns TRUE if the file is present in the array FALSE otherwise
+		 */
+		public function has_files(string $name = ""): bool
+		{
+			return (!empty($name) && isset($_FILES[$name])) || count($_FILES) > 0;
+		}
 
-    /**
-     * Private method used for parsing request body data for PUT and PATCH requests
-     *
-     * @return array Return an array of request body data
-     */
-    private function parse_patch_and_put_request_data(): array
-    {
+		/**
+		 * Method used for getting all request headers
+		 *
+		 * @return array It will return an array containing all the header values or an empty array
+		 */
+		private function get_request_headers(): array
+		{
+			if (function_exists("apache_request_headers")) {
+				$headers = apache_request_headers() ?? NULL;
+				return $headers ?? [];
+			}
 
-        /* PUT data comes in on the stdin stream */
-        $putData = fopen('php://input', 'r');
+			$headers = [];
+			foreach ($_SERVER as $key => $value) {
+				if (str_starts_with("HTTP_", $key)) {
+					$k = str_replace("HTTP_", "", $key);
+					$headers[$k] = $value;
+				}
+			}
 
-        $raw_data = '';
+			return $headers;
+		}
 
-        /* Read the data 1 KB at a time and write to the file */
-        while ($chunk = fread($putData, 1024))
-            $raw_data .= $chunk;
+		/**
+		 * Private method used for parsing request body data for PUT and PATCH requests
+		 *
+		 * @return array Return an array of request body data
+		 */
+		private function parse_patch_and_put_request_data(): array
+		{
 
-        /* Close the streams */
-        fclose($putData);
+			/* PUT data comes in on the stdin stream */
+			$putData = fopen('php://input', 'r');
 
-        // Fetch content and determine boundary
-        $boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
+			$raw_data = '';
 
-        if (empty($boundary)) {
-            parse_str($raw_data, $data);
-            return $data ?? [];
-        }
+			/* Read the data 1 KB at a time and write to the file */
+			while ($chunk = fread($putData, 1024))
+				$raw_data .= $chunk;
 
-        // Fetch each part
-        $parts = array_slice(explode($boundary, $raw_data), 1);
-        $data = [];
+			/* Close the streams */
+			fclose($putData);
 
-        foreach ($parts as $part) {
-            // If this is the last part, break
-            if ($part == "--\r\n") break;
+			// Fetch content and determine boundary
+			$boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
 
-            // Separate content from headers
-            $part = ltrim($part, "\r\n");
-            [$raw_headers, $body] = explode("\r\n\r\n", $part, 2);
+			if (empty($boundary)) {
+				parse_str($raw_data, $data);
+				return $data ?? [];
+			}
 
-            // Parse the headers list
-            $raw_headers = explode("\r\n", $raw_headers);
-            $headers = [];
-            foreach ($raw_headers as $header) {
-                [$name, $value] = explode(':', $header);
-                $headers[strtolower($name)] = ltrim($value, ' ');
-            }
+			// Fetch each part
+			$parts = array_slice(explode($boundary, $raw_data), 1);
+			$data = [];
 
-            // Parse the Content-Disposition to get the field name, etc.
-            if (isset($headers['content-disposition'])) {
-                preg_match(
-                    '/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/',
-                    $headers['content-disposition'],
-                    $matches
-                );
-                [, $type, $name] = $matches;
-                //Parse File
-                if (isset($matches[4])) {
-                    //if labeled the same as previous, skip
-                    if (isset($_FILES[$matches[2]])) {
-                        continue;
-                    }
+			foreach ($parts as $part) {
+				// If this is the last part, break
+				if ($part == "--\r\n") break;
 
-                    //get filename
-                    $filename = $matches[4] ?? NULL;
+				// Separate content from headers
+				$part = ltrim($part, "\r\n");
+				[$raw_headers, $body] = explode("\r\n\r\n", $part, 2);
 
-                    //get tmp name
-                    $filename_parts = pathinfo($filename);
-                    $tmp_name = tempnam(ini_get('upload_tmp_dir'), $filename_parts['filename']);
+				// Parse the headers list
+				$raw_headers = explode("\r\n", $raw_headers);
+				$headers = [];
+				foreach ($raw_headers as $header) {
+					[$name, $value] = explode(':', $header);
+					$headers[strtolower($name)] = ltrim($value, ' ');
+				}
 
-                    //populate $_FILES with information, size may be off in multibyte situation
-                    $_FILES[$matches[2]] = [
-                        'error' => 0,
-                        'name' => $filename,
-                        'tmp_name' => $tmp_name,
-                        'size' => strlen($body),
-                        'type' => $type,
-                    ];
+				// Parse the Content-Disposition to get the field name, etc.
+				if (isset($headers['content-disposition'])) {
+					preg_match(
+						'/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/',
+						$headers['content-disposition'],
+						$matches
+					);
+					[, $type, $name] = $matches;
+					//Parse File
+					if (isset($matches[4])) {
+						//if labeled the same as previous, skip
+						if (isset($_FILES[$matches[2]])) {
+							continue;
+						}
 
-                    //place in temporary directory
-                    file_put_contents($tmp_name, $body);
-                } else { //Parse Field
-                    $data[$name] = substr($body, 0, strlen($body) - 2);
-                }
-            }
-        }
-        return $data ?? [];
-    }
-}
+						//get filename
+						$filename = $matches[4];
+
+						//get tmp name
+						$filename_parts = pathinfo($filename);
+						$tmp_name = tempnam(ini_get('upload_tmp_dir'), $filename_parts['filename']);
+
+						//populate $_FILES with information, size may be off in multibyte situation
+						$_FILES[$matches[2]] = [
+							'error' => 0,
+							'name' => $filename,
+							'tmp_name' => $tmp_name,
+							'size' => strlen($body),
+							'type' => $type,
+						];
+
+						//place in temporary directory
+						file_put_contents($tmp_name, $body);
+					} else { //Parse Field
+						$data[$name] = substr($body, 0, strlen($body) - 2);
+					}
+				}
+			}
+			return $data ?? [];
+		}
+	}
